d497553978a3ac2073fc1f9c8fc57703c2862be8
João Beserra <joao.beserra@aluno.ifsp.edu.br>
2025-12-18 09:43:58 -0300

    first commit

---
+++ src/main/java/com/contam/ContamApplication.java
@@ -0,0 +1,13 @@
+package com.contam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/contam/ContamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
