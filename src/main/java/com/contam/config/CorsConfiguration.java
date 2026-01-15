db59950eeecef80b3997280499faa0f7737f1fda
João Beserra <joao.beserra@aluno.ifsp.edu.br>
2026-01-15 15:11:57 -0300

    feat: Configurango CORS para realizar testes com o frontend, nova migration com datas de crição, edição e deleção.

---
+++ src/main/java/com/contam/config/CorsConfiguration.java
@@ -0,0 +1,23 @@
+package com.contam.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:8081",
+                        "http://localhost:8082"
+                )
+                .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+
+}
